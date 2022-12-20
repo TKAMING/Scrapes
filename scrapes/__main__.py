b9b54cc74e59a8b74ef722337f47e166c477ff05
TKAMING <tobiaskaruth@gmail.com>
2022-12-20 19:57:21 +0100

    Bug

--- scrapes/__main__.py
+++ scrapes/__main__.py
@@ -4,7 +4,6 @@
 # imports
 import os
 import sys
-from datetime import datetime
 
 # banner for display
 banner = """
@@ -17,63 +16,20 @@ banner = """
 # help menu
 help_menu = """
         [+] Arguments:
-            
-        [+] Example:
-            scrapes ...
+            -r, --run ----------------- Run Scrapes
+            -v, --version ------------- Scrapes Version
+            -h, --help  --------------- Help Menu
+
+        [+] Usage:
+            scrapes [ARGUMENT]
 """
+
+header = f"[~] scrapes $ " # sets up user input interface
 
 
 # clear screen
 def clear():
     os.system("clear")
-
-# terminates program
-def exit():
-    print("\n[*] Exiting...")
-    sys.exit()
-
-# gets current date and time
-def current_date():
-    current = datetime.now()    
-
-    return current.strftime("%m-%d-%Y_%H-%M-%S")
-
-# update OnlyRAT
-def update():
-
-    print("\n[*] Checking for updates...")
-
-    # get latest version nubmer
-    os.system(f"curl https://raw.githubusercontent.com/TKAMING/Scrapes/main/version.txt | tee ~/.Scrapes/latest.txt")
-
-    # save version nubmers to memory
-    current_version = float(open(f"{local_path}/version.txt", "r").read())
-    latest_version = float(open(f"{local_path}/latest.txt", "r").read())
-
-    # remove version number file
-    os.system("rm -rf ~/.Scrapes/latest.txt")
-
-    # if new version is available, update
-    if latest_version > current_version:
-        print("\n[+] Update found")
-        print("[~] Update Scrapes? [y/n]\n")
-
-        # user input, option
-        option = input(f"{header}")
-        
-        # update
-        if option == "y":
-            os.system(f"bash ~/.Scrapes/payloads/update.sh")
-
-        # exception
-        # else:
-        #     main()
-
-    # otherwise, run main code
-    else:
-        print("\n[+] Scrapes already up to date")
-        print("[*] Hit any key to continue...\n")
-        input(header)
 
 # command line interface
 def cli(arguments):
@@ -82,37 +38,25 @@ def cli(arguments):
 
     print(banner)
 
-    # if arguments exist
-    if arguments:
-
-        argument = sys.argv[1]
-
-        if argument.endswith(".rat"):
-            print("\t[~] Type \"help\" for help menu :\n")
-
-            # loop user input
-            while True:
-
-                # user input, option
-                option = input(header)
-                rat_file = argument
-
 # main code
 def main():
-    
-    # clear screen
-    clear()
-
     # checks for arguments
-    try:
-        sys.argv[1]
-    except IndexError:
-        arguments_exist = False
-    else:
-        arguments_exist = True
+    #try:
+    args = sys.argv[:1]
+    #except IndexError:
+    #    arguments_exist = False
+    #else:
+    #    arguments_exist = True
 
-    # run command line interface
-    cli(arguments_exist)
+    if args == "--run" or args == "-r":
+        # run command line interface
+        cli()
+    
+    elif args == "--version" or args == "-v":
+       print(banner) 
+
+    elif args == "--help" or args == "-h":
+        print(help_menu)
 
 # runs main code
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 setup(
     name = 'Scrapes',
-    version = '0.1.0',
+    version = '1.0',
     packages = ['scrapes'],
+    license = "MIT License",
     entry_points = {
         'console_scripts': [
             'scrapes = scrapes.__main__:main'
