a71a88e31f471c79228b3d70dc82e765db0d3242
Tobias Karuth <98485613+TKAMING@users.noreply.github.com>
2022-12-21 06:21:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+# installer for Scrapes
+# created by : Tobias Karuth (TKAMING)
+
+#  get tools
+echo [*] Installing tools...
+sudo apt update
+sudo apt-get install python3
+sudo apt-get install pip3
+echo [+] Completed
+
+#installing
 pip3 install -e .
+
+# clean up
+echo [+] Installation Completed
+echo "- please restart your terminal"
+echo "- please type 'scrapes [ARGUMENT]' to launch Scrapes"
