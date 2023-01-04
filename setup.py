e6fced5224b700ab0a66155ef624f3558efb9d4f
TKAMING <tobiaskaruth@gmail.com>
2023-01-04 12:59:19 +0100

    test?

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version = 1.8,
     packages = ['scrapes'],
     license = "MIT License",
-    install_requires=['Chrome'],
+    #install_requires=['Chrome'],
     entry_points = {
         'console_scripts': [
             'scrapes = scrapes.__main__:main'
