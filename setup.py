f398fcda5bdc7d60969007bbf4fc2c77a5e40391
TKAMING <tobiaskaruth@gmail.com>
2023-01-04 12:57:15 +0100

    test

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 import os
 
-version = os.system("sudo curl https://raw.githubusercontent.com/TKAMING/Scrapes/main/version.txt")
+#version = os.system("sudo curl https://raw.githubusercontent.com/TKAMING/Scrapes/main/version.txt")
 
 setup(
     name = 'Scrapes',
-    version = version,
+    version = 1.8,
     packages = ['scrapes'],
     license = "MIT License",
     install_requires=['Chrome'],
